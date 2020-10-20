f16b0983d3c3c12e4eec949bd64a961d9665ed8d
Kalob98 <47434477+Kalob98@users.noreply.github.com>
2020-10-20 17:22:00 -0400

    Lists used to fill choiceBoxes

---
+++ APIs/cuisines.java
@@ -0,0 +1,58 @@
+package APIs;
+
+/**
+ * Class that creates a list of all cuisines spoonacular API offers to add to
+ * the cuisine choiceBox. The cuisine choiceBox can be left blank/null
+ *
+ * @author Kalob Reinholz
+ *
+ * Last updated 10/20/20
+ */
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class cuisines {
+
+    public static ObservableList loadCuisines() {
+        ObservableList<String> cuisineOptions = FXCollections.observableArrayList();
+        
+        cuisineOptions.removeAll(cuisineOptions);
+
+        String NULL = "";
+        String African = "African";
+        String American = "American";
+        String British = "British";
+        String Cajun = "Cajun";
+        String Caribbean = "Caribbean";
+        String Chinese = "Chinese";
+        String EasternEuropean = "Eastern European";
+        String European = "European";
+        String French = "French";
+        String German = "German";
+        String Greek = "Greek";
+        String Indian = "Indian";
+        String Irish = "Irish";
+        String Italian = "Italian";
+        String Japanese = "Japanese";
+        String Jewish = "Jewish";
+        String Korean = "Korean";
+        String LatinAmerican = "Latin American";
+        String Mediterranean = "Mediterranean";
+        String Mexican = "Mexican";
+        String MiddleEastern = "Middle Eastern";
+        String Nordic = "Nordic";
+        String Southern = "Southern";
+        String Spanish = "Spanish";
+        String Thai = "Thai";
+        String Vietnamese = "Vietnamese";
+        
+        cuisineOptions.addAll(NULL, African, American, British, Cajun, Caribbean, 
+                Chinese, EasternEuropean, European, French, German, Greek, 
+                Indian, Irish, Italian, Japanese, Jewish, Korean, LatinAmerican, 
+                Mediterranean, Mexican, MiddleEastern, Nordic, Southern, 
+                Spanish, Thai, Vietnamese);
+        
+        return cuisineOptions;
+    }
+}
---
+++ APIs/ingredients.java
@@ -0,0 +1,24 @@
+package APIs;
+
+/**
+ * Class that creates a list of some ingredients spoonacular API offers to add
+ * to the include and exclude choiceBoxes.
+ *
+ * @author Kalob Reinholz
+ *
+ * Last updated 10/20/20
+ */
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class ingredients {
+
+    public static ObservableList loadIngredients() {
+        ObservableList<String> ingredients = FXCollections.observableArrayList();
+        
+        
+        
+        return ingredients;
+    }    
+}
