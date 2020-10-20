ba4e2a0fd910d249242797281a95cde7712f6486
Kalob98 <47434477+Kalob98@users.noreply.github.com>
2020-10-20 17:21:25 -0400

    API

---
+++ APIs/apiKeys.java
@@ -0,0 +1,30 @@
+package APIs;
+
+/**
+ * Class that stores all api keys that the app uses. includes &apikey=
+ *
+ * @author Kalob Reinholz
+ *
+ * Last updated 10/20/20
+ */
+
+public class apiKeys {
+
+    public static String kalob_ApiKey(){
+        String kalob_ApiKey = "&apiKey=eb6531f544da4fce92deb6c85f667510";
+        
+        return kalob_ApiKey;
+    }
+    
+    public static String heng_ApiKey(){
+        String heng_ApiKey = "&apiKey=";
+        
+        return heng_ApiKey;
+    }
+    
+    public static String brody_ApiKey(){
+        String brody_ApiKey = "&apiKey=";
+        
+        return brody_ApiKey;
+    }
+}
