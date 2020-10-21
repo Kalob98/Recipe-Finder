92c20395082c62e3d3fd074711610e05bd7a898f
Kalob98 <47434477+Kalob98@users.noreply.github.com>
2020-10-20 23:48:03 -0400

    Added 12 ingredients

--- APIs/ingredients.java
+++ APIs/ingredients.java
@@ -8,7 +8,6 @@ package APIs;
  *
  * Last updated 10/20/20
  */
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -16,9 +15,26 @@ public class ingredients {
 
     public static ObservableList loadIngredients() {
         ObservableList<String> ingredients = FXCollections.observableArrayList();
+
+        ingredients.removeAll(ingredients);
         
+        String NULL = "";
+        String Almonds = "Almonds";
+        String Beef = "Beef";
+        String Cheese = "Cheese";
+        String Chicken = "Chicken";
+        String Eggs = "Eggs";
+        String Flour = "Flour";
+        String Milk = "Milk";
+        String Onions = "Onions";
+        String Pork = "Pork";
+        String Potatoes = "Potatoes";
+        String Rice = "Rice";
+        String Tomatoes = "Tomatoes";
         
-        
+        ingredients.addAll(NULL, Almonds, Beef, Cheese, Chicken, Eggs, Flour, 
+                Milk, Onions, Pork, Potatoes, Rice, Tomatoes);
+
         return ingredients;
-    }    
+    }
 }
