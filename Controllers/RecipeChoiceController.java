bfd3236da161f6473b38e21aa9b33ac171352b95
GrimmB96 <71104369+GrimmB96@users.noreply.github.com>
2020-10-27 01:10:00 -0400

    added recipe choice view/controller, alphabetized checkboxes

--- Controllers/RecipeChoiceController.java
+++ Controllers/RecipeChoiceController.java
@@ -7,7 +7,22 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 
 /**
  * FXML Controller class
@@ -15,6 +30,31 @@ import javafx.fxml.Initializable;
  * @author Brody
  */
 public class RecipeChoiceController implements Initializable {
+    
+    @FXML
+    private Label recipeOneLabel;
+    @FXML
+    private Label recipeTwoLabel;
+    @FXML
+    private Label recipeThreeLabel;
+    @FXML
+    private Label recipeFourLabel;
+    @FXML
+    private Label recipeFiveLabel;
+    @FXML
+    private Label recipeSixLabel;
+    @FXML
+    private Button recipeOneButton;
+    @FXML
+    private Button recipeTwoButton;
+    @FXML
+    private Button recipeThreeButton;
+    @FXML
+    private Button recipeFourButton;
+    @FXML
+    private Button recipeFiveButton;
+    @FXML
+    private Button recipeSixButton;
 
     /**
      * Initializes the controller class.
